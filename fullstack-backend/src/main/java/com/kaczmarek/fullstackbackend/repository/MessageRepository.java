8eb6679659defe28b4a83f24172b0c912b46c8b1
Jacek <jacek.kaczmarek@cornelsen.de>
2025-05-25 17:24:21 +0200

    refactor: change backend repository name

--- fullstack-backend/src/main/java/com/kaczmarek/fullstackbackend/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kaczmarek.fullstackbackend.repository;
-
-import com.kaczmarek.fullstackbackend.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
