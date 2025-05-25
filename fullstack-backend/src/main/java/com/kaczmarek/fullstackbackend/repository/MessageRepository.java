9747a2084382844e3c881d85e64e900fb4f437f2
Jacek <jacek.kaczmarek@cornelsen.de>
2025-05-25 10:20:46 +0200

    feat: update lombok version

--- fullstack-backend/src/main/java/com/kaczmarek/fullstackbackend/repository/MessageRepository.java
+++ fullstack-backend/src/main/java/com/kaczmarek/fullstackbackend/repository/MessageRepository.java
@@ -2,6 +2,8 @@ package com.kaczmarek.fullstackbackend.repository;
 
 import com.kaczmarek.fullstackbackend.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 }
